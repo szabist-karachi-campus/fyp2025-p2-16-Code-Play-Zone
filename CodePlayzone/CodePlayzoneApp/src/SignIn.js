ff03362c9dd82e0290dacfd3758f0152873be25a
ShahzaibKhan2343 <bcsbs2112343@szabist.pk>
2025-08-20 03:38:41 -0700

    FYP project Code Playzone added

--- CodePlayzone/CodePlayzoneApp/src/SignIn.js
+++ CodePlayzone/CodePlayzoneApp/src/SignIn.js
@@ -81,7 +81,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 20,
-    backgroundColor: 'black',
+    backgroundColor: '#FFF',
     justifyContent: 'center',
   },
   title: {
